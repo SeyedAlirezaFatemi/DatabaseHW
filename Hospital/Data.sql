c195fda7d868861f5898abcf4b68949aab5fcf33
alireza <alirezafa@rocketmail.com>
2019-04-14 20:48:21 +0430

    More Data.

---
+++ Hospital/Data.sql
@@ -0,0 +1,25 @@
+USE hospital;
+
+INSERT INTO doctor
+VALUES ('Ali', 'Fatemi', 1, 1);
+
+INSERT INTO doctor
+VALUES ('Reza', 'Kashfi', 2, 2);
+
+INSERT INTO doctor
+VALUES ('Mohammad', 'Kiani', 3, 3);
+
+INSERT INTO doctor
+VALUES ('Ahmad', 'Sarvari', 4, 4);
+
+INSERT INTO medicine
+VALUES (1, 'Acifluorfen', 'Aray', 100);
+
+INSERT INTO medicine
+VALUES (2, 'Fenoprofen', 'Trident', 200);
+
+INSERT INTO medicine
+VALUES (3, 'Actifen', 'Abidi', 300);
+
+INSERT INTO medicine
+VALUES (2, 'Butibufen', 'Aray', 400);
